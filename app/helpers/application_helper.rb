876f0695e1acb698083eb1091f75420ae300d761
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-08 21:05:03 +1200

    If a upload is viewed from a search, display a back link to results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+##
+# Application-wide helpers
 module ApplicationHelper
+  def from_search?
+    referer = URI.parse(request.referer)
+    referer.host == request.host && referer.path == search_uploads_path
+  rescue /bad URI/
+    false
+  end
 end
