702e65b70037ad04e53074bf4f7b8bb25243e6a8
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-10 21:02:55 +1200

    Rescue error class rather than error message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def from_search?
     referer = URI.parse(request.referer)
     referer.host == request.host && referer.path == search_uploads_path
-  rescue /bad URI/
+  rescue URI::InvalidURIError
     false
   end
 end
