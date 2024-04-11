4c02fde39f862c7dd241f97e82c41610aadd41b0
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-11 20:32:51 +1200

    Look for nearby elements with a segment ID if the selection does not have a segment ID
    This typically happens when the selection traverses a segment boundary - e.g. if a text selection or whitespace selection is made.

--- app/javascript/controllers/transcript_clip_controller.js
+++ app/javascript/controllers/transcript_clip_controller.js
@@ -3,16 +3,17 @@ import SelectionController from "controllers/selection_controller";
 export default class extends SelectionController {
   static targets = ["toolbar", "segmentStartInput", "segmentEndInput"];
 
+  resolveSegmentId(node) {
+    const elements = [node.parentElement, node.previousElementSibling, node.nextElementSibling];
+
+    return elements.find((element) => element?.dataset?.segmentId)?.dataset?.segmentId;
+  }
+
   selectionChanged(selection) {
-    if (selection.anchorNode.parentElement.dataset.segmentId) {
-      this.startElement = selection.anchorNode.parentElement;
-    }
+    let anchorNodeSegmentId = this.resolveSegmentId(selection.anchorNode) || this.resolveSegmentId(selection.focusNode);
+    let focusNodeSegmentId = this.resolveSegmentId(selection.focusNode) || this.resolveSegmentId(selection.anchorNode);
 
-    if (selection.focusNode.parentElement.dataset.segmentId) {
-      this.endElement = selection.focusNode.parentElement;
-    }
-
-    this.segmentStartInputTarget.value = this.startElement?.dataset?.segmentId;
-    this.segmentEndInputTarget.value = this.endElement?.dataset?.segmentId;
+    this.segmentStartInputTarget.value = anchorNodeSegmentId;
+    this.segmentEndInputTarget.value = focusNodeSegmentId;
   }
 }
