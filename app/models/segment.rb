2bebd89d64ad0d72d02806fb6a6cf840ba6a4e9d
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-11 20:35:22 +1200

    Extract breaks? method from template

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -2,4 +2,8 @@ class Segment < ApplicationRecord
   belongs_to :transcript
   has_one :upload, through: :transcript
   has_many :clips, foreign_key: :start_segment_id, dependent: :destroy
+
+  def breaks?
+    text.ends_with?(".") || text.ends_with?("?")
+  end
 end
