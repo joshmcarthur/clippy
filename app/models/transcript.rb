f5dda60208c6470e335c8f9e926cae12d9f4598f
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-11 20:30:56 +1200

    Order segments by start time

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -1,6 +1,6 @@
 class Transcript < ApplicationRecord
   belongs_to :upload
-  has_many :segments, dependent: :destroy
+  has_many :segments, -> { order(:range) }, dependent: :destroy
   has_many :clips, through: :segments
   has_one :summary, dependent: :destroy
 end
