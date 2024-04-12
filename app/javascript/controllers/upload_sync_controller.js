d037390e9f6d7fd976198b7eac07d27f45a7b602
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-12 16:40:41 +1200

    Refactor seeking to use hash so that we can link to a time nicely

--- app/javascript/controllers/upload_sync_controller.js
+++ app/javascript/controllers/upload_sync_controller.js
@@ -4,32 +4,45 @@ export default class extends Controller {
   static targets = ["media", "transcript"];
 
   connect() {
-    this.mediaTarget.addEventListener("timeupdate", () => {
-      const currentTime = this.mediaTarget.currentTime;
-      const segments = this.transcriptTarget.querySelectorAll(".transcript-segment");
+    this.mediaTarget.addEventListener("timeupdate", this.updateProgress.bind(this));
+    window.addEventListener("hashchange", this.handleHashChange.bind(this));
+    this.handleHashChange();
+  }
 
-      segments.forEach((segment) => {
-        const startTime = parseFloat(segment.dataset.starts);
-        const endTime = parseFloat(segment.dataset.ends);
+  handleHashChange() {
+    const hash = window.location.hash;
+    console.log(hash);
+    if (hash.startsWith("#t=")) {
+      const seconds = parseInt(hash.substring(3, hash.length - 1));
+      this.mediaTarget.currentTime = seconds;
+    }
+  }
 
-        if (startTime < currentTime) {
-          segment.classList.add("opacity-50");
-        } else {
-          segment.classList.remove("opacity-50");
+  updateProgress() {
+    const currentTime = this.mediaTarget.currentTime;
+    const segments = this.transcriptTarget.querySelectorAll("[data-starts][data-ends]");
+
+    segments.forEach((segment) => {
+      const startTime = parseFloat(segment.dataset.starts);
+      const endTime = parseFloat(segment.dataset.ends);
+
+      if (startTime < currentTime) {
+        segment.classList.add("opacity-50");
+      } else {
+        segment.classList.remove("opacity-50");
+      }
+
+      if (startTime < currentTime && currentTime < endTime) {
+        const scrollThreshold = 500; // Adjust this value to determine the scroll threshold
+
+        if (Math.abs(segment.getBoundingClientRect().top) < scrollThreshold) {
+          segment.scrollIntoView({ block: "start" });
         }
 
-        if (startTime < currentTime && currentTime < endTime) {
-          const scrollThreshold = 500; // Adjust this value to determine the scroll threshold
-
-          if (Math.abs(segment.getBoundingClientRect().top) < scrollThreshold) {
-            segment.scrollIntoView({ block: "start" });
-          }
-
-          segment.classList.add("link-underline-opacity-75");
-        } else {
-          segment.classList.remove("link-underline-opacity-75");
-        }
-      });
+        segment.classList.add("link-underline-opacity-75");
+      } else {
+        segment.classList.remove("link-underline-opacity-75");
+      }
     });
   }
 
