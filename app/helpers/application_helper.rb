02fda395f0c50bd1ddd6d854349d073227ee267e
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-12 16:41:06 +1200

    Add helpers to describe play time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
   rescue URI::InvalidURIError
     false
   end
+
+  def describe_play_time(seconds)
+    Time.at(seconds).utc.strftime("%H:%M:%S")
+  end
+
+  def describe_play_time_range(from_seconds, to_seconds)
+    [describe_play_time(from_seconds), describe_play_time(to_seconds)].join(" - ")
+  end
 end
