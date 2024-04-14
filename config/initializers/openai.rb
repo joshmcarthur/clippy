73bf52ecd3312e836df1656d7d00f1846c904416
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-14 20:41:43 +1200

    Load OpenAI key from an environment variable if the credential is not configured

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,4 @@
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai.fetch(:access_token)
-  config.organization_id = Rails.application.credentials.openai[:organization_id]
+  config.access_token = Rails.application.credentials.openai.fetch(:access_token, ENV["OPENAI_API_KEY"])
+  config.organization_id = Rails.application.credentials.openai[:organization_id] || ENV["OPENAI_ORGANIZATION_ID"]
 end
