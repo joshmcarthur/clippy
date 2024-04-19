a4350768bdcb00557a65705ebaac1c96e8bde802
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-19 14:45:03 +1200

    Store databases in the storage directory

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+ENV["LITESTACK_DATA_PATH"] ="./storage/database"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
