cffd3a9fefcc89f2a350b5118038bb9433bf6c1d
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-19 14:29:06 +1200

    Make OpenAI initialisation threadsafe and more resilient to ENV based config

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,4 +1,6 @@
-OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.dig("openai.access_token") || ENV["OPENAI_API_KEY"]
-  config.organization_id = Rails.application.credentials.dig("openai.organization_id") || ENV["OPENAI_ORGANIZATION_ID"]
+Rails.application.config.to_prepare do
+  OpenAI.configure do |config|
+    config.access_token = Rails.application.credentials.fetch(:openai, {}).fetch(:access_token, ENV["OPENAI_API_KEY"])
+    config.organization_id = Rails.application.credentials.fetch(:openai, {}).fetch(:organization_id, ENV["OPENAI_ORGANIZATION_ID"])
+  end
 end
