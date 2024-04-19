6d7a332151e07cc93e751afa14a8536f2290e1f8
Josh McArthur <joshua.mcarthur@gmail.com>
2024-04-19 14:30:04 +1200

    Remove route to GoodJob

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "healthcheck" => "rails/health#show", as: :rails_health_check
-  mount GoodJob::Engine => '/admin/jobs'
 
 
   # Defines the root path route ("/")
